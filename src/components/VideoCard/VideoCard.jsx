6554534655a1200258f7f489140fb9ecf76c6057
Dev-Maradona <maradona.0000@yahoo.com>
2023-02-06 14:52:52 +0200

    Create Videos Components

---
+++ src/components/VideoCard/VideoCard.jsx
@@ -0,0 +1,9 @@
+const VideoCard = () => {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
+
+export default VideoCard;
--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,8 +1,22 @@
-const Videos = ({ videos }) => {
+import Loader from "../Loader/Loader";
+import { Stack, Box } from "@mui/material";
+import VideoCard from "../VideoCard/VideoCard";
+
+const Videos = ({ videos, direction }) => {
+
+    if(!videos?.length) return <Loader />
+
     return (
-        <div>
-            Hello World!
-        </div>
+        <Stack
+        // direction={direction || "row"}
+        flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+            { videos.map((item, index) => (
+                <Box key={index}>
+                    {item.id.videoId && <VideoCard />}
+                    {/* {item.id.channelId && <ChannelCard />} */}
+                </Box>
+            )) }
+        </Stack>
     )
 }
 
